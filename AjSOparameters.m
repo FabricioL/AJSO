edc4f88d1a1c42d5eaf78d76bb838fd1f2dac117
LAPTOP-NLVE0QKH\LenovoF <loorfabricio@gmail.com>
2021-03-10 10:21:44 -0300

    New Init

---
+++ AjSOparameters.m
@@ -0,0 +1,15 @@
+% Author:           Rainer Storn, Ken Price, Arnold Neumaier, Jim Van Zandt
+% Modified by FLC \GECAD 04/winter/2017
+
+deParameters.I_NP= 10; % population in DE
+deParameters.I_F = 0.99; %Mutation factor
+deParameters.I_CR = 0.45; %Recombination constant
+deParameters.I_evalmax= 5000; % number of max iterations/gen
+deParameters.g_p_best_rate = 0.1;
+
+% Testbed 2
+% deParameters.I_NP= 100; % population in DE
+% deParameters.I_F = 0.8; %Mutation factor
+% deParameters.I_CR = 0.5; %Recombination constant
+% deParameters.I_evalmax= 50000; % number of max iterations/gen
+% deParameters.g_p_best_rate = 0.1;
